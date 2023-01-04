969c39d862e01677e11a36ed9eecd127b70f3009
kawksung <alstjd5479@naver.com>
2023-01-04 10:02:36 +0900

    Update LV1 약수의 합.py

--- LV1 약수의 합.py
+++ LV1 약수의 합.py
@@ -1,2 +1,18 @@
-a=123
-print(str(a).split().reverse)
+import math
+def solution(n):
+    answer = 0
+    for i in range(1,int(math.sqrt(n))+1):
+        for j in range(i,n+1):
+            if i*j==n:
+                if i==j:
+                    answer+=i
+                else:
+                    answer+=i+j
+                continue
+                
+    return answer
+
+'''
+def solution(n):
+    return n + sum([i for i in range(1, (n // 2) + 1) if n % i == 0])
+'''
