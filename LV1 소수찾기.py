6349ca9355d6f7b8e99139c4f5d2e9fff0ee4d7b
kawksung <alstjd5479@naver.com>
2023-01-06 00:24:39 +0900

    Create LV1 소수찾기.py

---
+++ LV1 소수찾기.py
@@ -0,0 +1,21 @@
+
+def primenumber(x):
+    for i in range(2, int(x**.5) + 1):
+        if x % i == 0:
+            return False
+        return True
+
+
+def solution(n):
+    return len([i for i in range(2, n+1) if primenumber(i)])
+
+
+'''
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,int(n**.5)+1):
+        if i in num:
+            num-=set(range(i*i,n+1,i))
+    return len(num)
+'''
