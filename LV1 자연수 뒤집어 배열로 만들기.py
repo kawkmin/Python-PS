1d951c7043eb570638340a284154905a32c60fd8
kawksung <alstjd5479@naver.com>
2023-01-12 22:37:32 +0900

    Update LV1 자연수 뒤집어 배열로 만들기.py

--- LV1 자연수 뒤집어 배열로 만들기.py
+++ LV1 자연수 뒤집어 배열로 만들기.py
@@ -1,11 +1,12 @@
 def solution(n):
-    temp_list=list(str(n)[::-1])
-    answer=[]
+    temp_list = list(str(n)[::-1])
+    answer = []
     for i in temp_list:
         answer.append(int(i))
     return answer
 
-'''
+
+"""
 def digit_reverse(n):
     return list(map(int, reversed(str(n))))
-'''
+"""
